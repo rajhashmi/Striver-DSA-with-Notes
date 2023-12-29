9ba7e7d95eee4a8d287f32ee0a60b30cc341cceb
shahid <shahidhashmiraj@gmail.com>
2023-12-30 02:30:21 +0530

    find peak solution

--- __Binary__/app.js
+++ __Binary__/app.js
@@ -103,3 +103,24 @@ var singleNonDuplicate = function(nums) {
 
     return nums[low];
 };
+
+
+
+
+// Find Peak Element
+
+var findPeakElement = function(nums) {
+    if(nums.length == 1) return 0;
+    if(nums[0] > nums[1]) return 0;
+    if(nums[nums.length -1] > nums[nums.length - 2]) return nums.length - 1;
+    let low = 1, high = nums.length -2;
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2);
+
+        if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) return mid;
+
+        if(nums[mid] > nums[mid - 1]) low = mid + 1;
+        else high = mid - 1
+    }
+    return -1
+};
